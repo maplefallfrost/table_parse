722ea644cd722fb688301202d8f0c6ad8353cf72
junyu.ye <maplefallfrost@gmail.com>
2024-03-30 10:17:08 +0900

    refactor interface

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # table_parse
+
+## Usage
+```python main.py --mode=connected_component --pdf_path=data/table_sample.pdf --output_dir=output```
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import argparse
 import os
 
 from modules.table import parse_image
 
-mode = "connected_component"
-pdf_path = "data/table_sample.pdf"
-output_dir = "output"
 
-
-def main():
-    os.makedirs(output_dir, exist_ok=True)
-    parse_image(mode, pdf_path, output_dir)
+def main(args: argparse.Namespace):
+    os.makedirs(args.output_dir, exist_ok=True)
+    parse_image(args.mode, args.pdf_path, args.output_dir)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mode",
+        type=str,
+        required=True,
+        choices=["connected_component", "textractor", "unstructured", "google_doc"],
+    )
+    parser.add_argument("--pdf_path", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, default="output")
+    args = parser.parse_args()
+    main(args)
