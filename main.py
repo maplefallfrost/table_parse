44189c56d8474e64905edf0d82444a6bf42b9afb
Junyu Ye <maplefallfrost@gmail.com>
2024-03-30 09:00:44 +0900

    create output_dir if it is not existed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from modules.table import parse_image
 
 mode = "connected_component"
@@ -6,6 +8,7 @@ output_dir = "output"
 
 
 def main():
+    os.makedirs(output_dir, exist_ok=True)
     parse_image(mode, pdf_path, output_dir)
 
 
